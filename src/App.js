12c28f31fd01b4209d1f11d5b918c6b73bf7ead1
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-09-07 16:21:42 -0400

    Lay out new data set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { stratify, hierarchy } from 'd3-hierarchy';
 import StockTreemap from './components/StockTreemap';
 import data from './data/data.json';
 
+const stratifiedData = stratify()
+  .id(d => d.id)
+  .parentId(d => d.parent)(data)
+  .sum(d => d.billions);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="App" style={{ margin: '1rem auto', maxWidth: '1024px' }}>
-        <StockTreemap data={data} height="600" width="800" />
+        <StockTreemap data={stratifiedData} height="600" width="800" />
       </div>
     );
   }
--- src/components/StockTreemap.js
+++ src/components/StockTreemap.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { hierarchy, treemap as d3treemap, treemapBinary } from 'd3-hierarchy';
+import { treemap as d3treemap, treemapBinary } from 'd3-hierarchy';
 import { format } from 'd3-format';
 import Tooltip from './Tooltip';
 
@@ -15,56 +15,11 @@ export const formatter = (number, type) => {
   }
 };
 
-const dynamicInnerPadding = node => {
-  switch (node.depth) {
-    case 0:
-      return 12;
-    case 1:
-      return 3;
-    default:
-      return 1;
-  }
-};
-
-const nodeLabel = node => {
-  let nodeCopy = Object.assign({}, node);
-  let label = nodeCopy.data.name;
-
-  while (nodeCopy.parent !== null) {
-    nodeCopy = Object.assign({}, nodeCopy.parent);
-
-    switch (nodeCopy.depth) {
-      case 0:
-        break;
-      case 1:
-        label = `<h3>${nodeCopy.data.name} Funds</h3><p>${label}`;
-        break;
-      default:
-        label = `${nodeCopy.data.name}, ${label}`;
-        break;
-    }
-  }
-
-  label = `<div>${label}</p></div>`;
-  return label;
-};
-
-const getGroup = node => {
-  let nodeCopy = Object.assign({}, node);
-
-  while (nodeCopy.parent !== null && nodeCopy.parent.depth >= 1) {
-    nodeCopy = Object.assign({}, nodeCopy.parent);
-  }
-
-  return nodeCopy.data.name;
-};
-
 class StockTreemap extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      root: hierarchy(this.props.data).sum(d => d.billions),
       selectedGroup: null,
     };
   }
@@ -77,42 +32,10 @@ class StockTreemap extends Component {
     const treemap = d3treemap()
       .size([this.props.width, this.props.height])
       .tile(treemapBinary)
-      .paddingInner(dynamicInnerPadding);
-    const nodes = treemap(this.state.root).descendants();
-
-    const color = (group, active) => {
-      const colors = {
-        domestic: {
-          active: '#8bc34a',
-          inactive: '#c5e1a5',
-        },
-        'non-domestic': {
-          active: '#00bcd4',
-          inactive: '#80deea',
-        },
-        'not-reported': {
-          active: '#eceff1',
-          inactive: '#fafafa',
-        },
-      };
-
-      return active ? colors[group].active : colors[group].inactive;
-    };
+      .paddingInner(2);
+    const nodes = treemap(this.props.data).descendants();
 
     const rects = nodes.map((node, i) => {
-      const group = getGroup(node);
-
-      const tooltipProps = node.children
-        ? null
-        : {
-            'data-tip': `${nodeLabel(node)}<p><strong>${formatter(
-              node.value,
-              '$'
-            )} billion</strong></p>`,
-            'data-for': 'treemap-tooltip',
-            'data-html': true,
-          };
-
       if (node.children === undefined) {
         return (
           <rect
@@ -120,33 +43,10 @@ class StockTreemap extends Component {
             y={node.y0}
             width={node.x1 - node.x0}
             height={node.y1 - node.y0}
-            fill={
-              node.data.group
-                ? color(node.data.group, this.state.selectedGroup === group)
-                : 'transparent'
-            }
             key={`node-${i}`}
-            onMouseEnter={() => this.onRectHover(group)}
+            onMouseEnter={() => this.onRectHover(node.parent.data.id)}
             onMouseLeave={() => this.onRectHover(null)}
-            {...tooltipProps}
           />
-        );
-      } else {
-        return null;
-      }
-    });
-
-    const texts = nodes.map(node => {
-      const group = getGroup(node);
-
-      if (node.depth === 1 && group !== this.state.selectedGroup) {
-        return (
-          <text
-            style={{ fontFamily: '"Lato", sans-serif' }}
-            textAnchor="middle"
-            x={node.x0 + (node.x1 - node.x0) / 2}
-            y={node.y0 + (node.y1 - node.y0) / 2}
-          >{`${node.data.name} Funds`}</text>
         );
       } else {
         return null;
@@ -160,7 +60,6 @@ class StockTreemap extends Component {
           viewBox={`0 0 ${this.props.width} ${this.props.height}`}
         >
           {rects}
-          {texts}
         </svg>
         <Tooltip id="treemap-tooltip" aria-haspopup="true" />
       </Fragment>
